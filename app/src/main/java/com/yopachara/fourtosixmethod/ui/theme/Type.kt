118959198097aba7ff9406c8efdb3a7821113793
yopachara <yopachara@gmail.com>
2021-07-26 23:53:26 +0700

    update font style

--- app/src/main/java/com/yopachara/fourtosixmethod/ui/theme/Type.kt
+++ app/src/main/java/com/yopachara/fourtosixmethod/ui/theme/Type.kt
@@ -4,6 +4,7 @@ import androidx.compose.material.Typography
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
@@ -11,8 +12,9 @@ import androidx.compose.ui.unit.sp
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 16.sp,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
     )
     /* Other default text styles to override
     button = TextStyle(
@@ -31,8 +33,9 @@ val Typography = Typography(
 val TypographyDark = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
         color = Color.White
     )
 )

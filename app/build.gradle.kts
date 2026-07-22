101dde6cdaf713eb8b67c1f19472ccdd56c8cac4
yopachara <yopachara@gmail.com>
2026-07-22 16:32:12 +0700

    chore(app): bump version to 1.2 (3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ val keystoreProperties = Properties().apply {
 android {
     defaultConfig {
         applicationId = "com.yopachara.fourtosixmethod"
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
